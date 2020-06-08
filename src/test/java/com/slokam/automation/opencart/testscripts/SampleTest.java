832d979f2b9eabfa10cfe499c1a809803f6687b2
Sanskar Modi <50732690+Sanskar-Modi@users.noreply.github.com>
2020-06-08 15:54:25 +0530

    Removed Firefox

--- src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
+++ src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
@@ -31,22 +31,6 @@ public class SampleTest {
 		System.out.println("End of my testcase - testChrome " + System.currentTimeMillis());
 	}
 
-	@Test
-	public void testFirefox() {
-		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
-		System.out.println("I am testFirefox");
-		Capabilities cap = DesiredCapabilities.firefox();
-		URL url = null;
-		try {
-			url = new URL("http://localhost:4444/wd/hub");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		WebDriver driver = new RemoteWebDriver(url,cap);
-		driver.get("http://localhost/opencart/");
-		driver.quit();
-		System.out.println("End of my testcase - testFirefox " + System.currentTimeMillis());
-	}
 
 	@Test
 	public void testChrome1() {
@@ -66,23 +50,6 @@ public class SampleTest {
 	}
 
 	@Test
-	public void testFirefox1() {
-		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
-		System.out.println("I am testFirefox1");
-		Capabilities cap = DesiredCapabilities.firefox();
-		URL url = null;
-		try {
-			url = new URL("http://localhost:4444/wd/hub");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		WebDriver driver = new RemoteWebDriver(url,cap);
-		driver.get("http://localhost/opencart/");
-		driver.quit();
-		System.out.println("End of my testcase - testFirefox1 " + System.currentTimeMillis());
-	}
-
-	@Test
 	public void testChrome2() {
 		System.out.println("Starting the testcase - testChrome " + System.currentTimeMillis());
 		System.out.println("I am testChrome2");
@@ -99,20 +66,5 @@ public class SampleTest {
 		System.out.println("End of my testcase - testChrome2 " + System.currentTimeMillis());
 	}
 
-	@Test
-	public void testFirefox2() {
-		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
-		System.out.println("I am testFirefox2");
-		Capabilities cap = DesiredCapabilities.firefox();
-		URL url = null;
-		try {
-			url = new URL("http://localhost:4444/wd/hub");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		WebDriver driver = new RemoteWebDriver(url,cap);
-		driver.get("http://localhost/opencart/");
-		driver.quit();
-		System.out.println("End of my testcase - testFirefox2 " + System.currentTimeMillis());
-	}
+
 }
